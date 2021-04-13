30bae010ddebf065c462eaf2b8f213b66f5e989d
Matus Poruben <poruben.matus@gmail.com>
2021-04-13 17:11:14 +0200

    plugins

--- plugins/Header.vue
+++ plugins/Header.vue
@@ -1,10 +1,31 @@
 <template>
   <div id="top">
-
+    <b-navbar id="top_bar" toggleable="lg" type="dark" class="shadow-lg">
+      <b-navbar-brand href="/">Matthew Poruben</b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-navbar-nav>
+            <b-nav-item href="/">Home</b-nav-item>
+            <b-nav-item href="/about">About</b-nav-item>
+          </b-navbar-nav>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
   </div>
 </template>
 
-<script lang="ts">
-import Vue from 'vue'
-export default Vue.extend({})
-</script>
+<style lang="sass" scoped>
+  #top
+    width: 100%
+    position: fixed
+    display: flex
+    align-content: center
+    justify-content: center
+    z-index: 100
+    #top_bar
+      margin-top: 10px
+      width: 90%
+      border-radius: 10px
+      background: #394053
+</style>
