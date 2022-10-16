6524fa4c85d4fabe032a3e381a2817ecd1dc030d
matusporuben <matus.poruben@gmail.com>
2022-10-16 16:34:48 +0200

    cleanups

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,6 +8,19 @@ export default defineNuxtConfig({
   
   css: ['~/theme/main.sass'],
 
+  head: {
+    htmlAttrs: {
+      lang: 'en'
+    },
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+
   components: [
     {
       path: '~/components/pageWidgets',
