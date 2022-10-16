08d6d546b108849dfe6f6e1f90f854ab03afbbed
matusporuben <matus.poruben@gmail.com>
2022-10-16 16:19:16 +0200

    move content component to content folder

---
+++ src/components/content/HomeDescription.vue
@@ -0,0 +1,58 @@
+<template>
+  <div id="descContainer">
+    <div id="desc" class="shadow-lg">
+      <div id="photo" :style="descriptionStyleImage"></div>
+      <div id="text">
+        <p>
+          <slot></slot>
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import {computed} from 'vue'
+import {useImage} from "#image/composables";
+
+const img = useImage()
+
+const descriptionStyleImage = computed(() =>
+  ({
+    backgroundImage: `url('/images/me.webp')`
+  })
+)
+</script>
+
+<style lang="sass" scoped>
+#descContainer
+  width: 100%
+  display: flex
+  justify-content: center
+  align-items: center
+  #desc
+    background: #394053
+    max-width: 1200px
+    width: 90%
+    height: 500px
+    border-radius: 30px
+    display: grid
+    overflow: hidden
+    grid-template-columns: 400px auto
+    @media only screen and (max-width: 900px)
+      grid-template-columns: auto
+      grid-template-rows: 500px auto
+      height: auto
+    #photo
+      border-radius: 30px
+      background-size: cover
+      background-position: center
+    #text
+      padding: 30px
+      font-size: 1.5em
+      display: flex
+      align-items: center
+      justify-content: center
+      @media only screen and (max-width: 900px)
+        font-size: 1.2em
+</style>
