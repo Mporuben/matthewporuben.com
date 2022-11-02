d6a7a0df7dbf481fddbcdc8559e4af3e8aac8506
matusporuben <matus.poruben@gmail.com>
2022-11-02 18:24:10 +0100

    fix google analytics not track in dev

--- src/plugins/googleAnalytics.ts
+++ src/plugins/googleAnalytics.ts
@@ -2,7 +2,10 @@ import VueGtag from 'vue-gtag'
 
 const id = process.env.GOOGLE_ANALYTICS_ID
 export default defineNuxtPlugin((nuxtApp) => {
-  nuxtApp.vueApp.use(VueGtag, {
-    config: {id},
-  }, nuxtApp.$router)
+  if (process.env.NODE_ENV !== 'development') {
+    nuxtApp.vueApp.use(VueGtag, {
+      config: {id},
+    }, nuxtApp.$router)
+  }
+
 })
