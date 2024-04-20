075339f029e521145c923db64b2fb8a3498b8c78
matusporuben <matus.poruben@gmail.com>
2024-04-20 15:34:23 +0200

    fix menu

--- app.vue
+++ app.vue
@@ -1,13 +1,17 @@
 <template>
   <div id="page">
     <div id="top">
-      <header id="top_bar">
-        <router-link to='/'>
-          <strong>Matthew Poruben</strong>
-        </router-link>
-        <div id="navigation">
+      <header id="top_bar" :class="{expanded: expandedMenu}">
+        <div id="title">
+          <router-link to='/'>
+            <strong>Matthew Poruben</strong>
+          </router-link>
+        </div>
+
+        <div id="navigation" >
           <ContentNavigation v-slot="{ navigation }">
             <router-link
+              @click="closeMenu"
               v-for="link of navigation"
               :key="link._path"
               :to="link._path"
@@ -19,6 +23,11 @@
             </router-link>
           </ContentNavigation>
         </div>
+
+        <div id="burger-menu" @click="toggleMenu">
+          <img  src="/icons/menu-outline.svg" alt="menu" style="width: 50px; height: 50px;"/>
+        </div>
+
       </header>
     </div>
     <NuxtPage/>
@@ -28,6 +37,20 @@
     </footer>
   </div>
 </template>
+<script lang="ts" setup>
+
+const expandedMenu = ref(false)
+
+const toggleMenu = () => {
+  expandedMenu.value = !expandedMenu.value
+}
+const closeMenu = () => {
+  expandedMenu.value = false
+}
+
+
+</script>
+
 
 <style scoped lang="sass">
 #page
@@ -35,28 +58,41 @@
   #top
     width: 100%
     display: flex
-    align-content: center
+    align-items: center
     justify-content: center
     position: fixed
     z-index: 100
     #top_bar
-      padding: 10px 20px
+      padding: 0px 20px
       width: 95%
+      height: 80px
+      display: grid
+      grid-template-columns: auto auto
+      grid-template-rows: 80px auto
+      grid-template-areas: 'title navigation' 'expand expand'
       max-width: 1000px
       margin-top: 10px
       box-shadow: 5px 5px 20px rgba(0,0,0,0.4)
       border-radius: 40px
       background: var(--dark-lighter)
-      display: flex
-      align-items: center
-      justify-content: space-between
+      #burger-menu
+        display: none
       #navigation
+        width: 100%
+        align-items: center
+        height: 100%
         display: flex
+        grid-area: navigation
+        justify-content: flex-end
         flex-flow: row wrap
-      strong
-        color: white
+      #title
         font-size: 1.2em
         line-height: 0
+        grid-area: title
+        display: flex
+        align-items: center
+        strong
+          color: white
       .link
         color: white
         text-decoration: none
@@ -65,8 +101,32 @@
         transition: 0.5s ease
         &:hover
           color: rgba(255,255,255,0.5)
-      .active
-        color: var(--primary)
+    @media only screen and (max-width: 600px)
+      #top_bar
+        transition: 0.3s ease-in-out  all
+        #navigation
+          grid-area: expand
+          transition: 0.2s all
+          visibility: hidden
+          display: flex
+          justify-content: flex-start
+          align-items: flex-end
+          flex-direction: column
+          opacity: 0
+        #burger-menu
+          grid-area: navigation
+          display: flex
+          justify-content: flex-end
+          align-items: center
+        &.expanded
+          height: 280px
+          #navigation
+            opacity: 1
+            transition: 0.4s ease-in all
+            visibility: visible
+            grid-area: expand
+            width: 100%
+            height: 100%
   footer
     width: 100%
     text-align: center
